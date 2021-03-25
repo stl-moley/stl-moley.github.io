fdcca0e86b62c0832a5151007510f656e0eded12
stl-moley <75780196+stl-moley@users.noreply.github.com>
2021-03-25 14:42:14 +0100

    Adding BIL Project
    Soutenance WF3

---
+++ BIL-BeInLink/js/main.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector('.toggle');
+const body = document.querySelector('body');
+toggle.addEventListener('click', function() {
+    body.classList.toggle('open');
+});
